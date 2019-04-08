172e2a022804d0d3a8e2b6e9fb8acd94a0d9775e
ahmadreza <arjaame@gmail.com>
2019-04-08 19:51:42 +0200

    timepicker is working now

--- app/src/main/java/com/example/lab2firebase/EditRestaurantProfile.java
+++ app/src/main/java/com/example/lab2firebase/EditRestaurantProfile.java
@@ -10,18 +10,33 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.internal.api.FirebaseNoSignedInUserException;
 
 public class EditRestaurantProfile extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
+    private DatabaseReference reference;
+    private TextView txt1,txt2,txt3,txt4;
+    private Button savebutton;
+
     int i, j, k, l;
     boolean a,b,c,d;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_restaurant_profile);
+
+        txt1=(TextView) findViewById(R.id.txthourfrom);
+        savebutton=(Button) findViewById(R.id.btnsave);
+        reference=FirebaseDatabase.getInstance().getReference();
         Button btntimepicker1, btntimepicker2, btntimepicker3, btntimepicker4;
         final DialogFragment timepicker1, timepicker2, timepicker3, timepicker4;
         a=b=c=d=false;
@@ -39,7 +54,7 @@ public class EditRestaurantProfile extends AppCompatActivity implements TimePick
         btntimepicker2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {  a=false;
-            b=true;
+                b=true;
                 timepicker2.show(getSupportFragmentManager(), "timepicker");
             }
         });
@@ -57,6 +72,18 @@ public class EditRestaurantProfile extends AppCompatActivity implements TimePick
                 timepicker4.show(getSupportFragmentManager(), "timepicker");
             }
         });
+        savebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v==savebutton) {
+                    storeuserinf();
+                }
+            }
+        });
+
+
+
+
         //*********Toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -86,8 +113,8 @@ public class EditRestaurantProfile extends AppCompatActivity implements TimePick
     //End of code related to the toolbar
 
 
-        @Override
-        public void onTimeSet (android.widget.TimePicker View,int hourOfDay, int minute){
+    @Override
+    public void onTimeSet (android.widget.TimePicker View,int hourOfDay, int minute){
         i = hourOfDay;
         j = minute;
         TextView textView1 = (TextView) findViewById(R.id.txthourfrom);
@@ -95,17 +122,28 @@ public class EditRestaurantProfile extends AppCompatActivity implements TimePick
         TextView textView3=(TextView) findViewById(R.id.txthourfrom2);
         TextView textView4=(TextView) findViewById(R.id.txthourto2);
         if (a==true){
-        textView1.setText("From:  "+'\n'+ hourOfDay+":" + minute);
+            textView1.setText("From:  "+'\n'+ hourOfDay+":" + minute);
 
-    }   else if (b==true){
-         textView2.setText(" To  " +'\n'+ hourOfDay+":" + minute);
+        }   else if (b==true){
+            textView2.setText(" To  " +'\n'+ hourOfDay+":" + minute);
         }     else if (c==true) {
             textView3.setText("From:  "+'\n'+ hourOfDay+":" + minute);
         }else if (d==true){
-                textView4.setText("To:  "+'\n'+ hourOfDay+":" + minute);
-            }
-            else return;
+            textView4.setText("To:  "+'\n'+ hourOfDay+":" + minute);
         }
+        else return;
     }
+    private void  storeuserinf()
+    {
+        String hour=txt1.getText().toString().trim();
+        saveinformation saveinformation=new saveinformation(hour);
+        reference.child("child").setValue(saveinformation);
+        Toast.makeText(this,hour,Toast.LENGTH_SHORT).show();
+
+
+    }
+
+
+}
 
 
---
+++ app/src/main/java/com/example/lab2firebase/saveinformation.java
@@ -0,0 +1,13 @@
+package com.example.lab2firebase;
+
+public class saveinformation {
+    public String hourfrom;
+    public saveinformation(){
+
+    }
+
+    public saveinformation(String hourfrom) {
+        this.hourfrom = hourfrom;
+    }
+
+}
